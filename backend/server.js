8a20649da40278d6275d600e193cab6b17dc2e17
Rhogenn <91942812+ManaChanCoder@users.noreply.github.com>
2025-08-18 19:59:44 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,30 +10,12 @@ const app = express();
 dotenv.config()
 
 // middleware
-app.use(express.json());
-const allowedOrigins = [
- "https://todo-app-rose-omega.vercel.app",
-  "http://localhost:1500", // optional: local dev
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
+    origin: ["https://todo-app-rose-omega.vercel.app", "http://localhost:1500"],
   })
 );
-
-// handle preflight requests (OPTIONS)
-app.options("*", cors());
-
+app.use(express.json());
 
 // API Routes
 app.use("/api/todos", todosRoute);
